6f42185c74aa4fe177f7c9abd1f8ae67abaae9cd
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-06-30 19:04:01 +0300

    feat: display only a single daily quote based on date

--- javascript.js
+++ javascript.js
@@ -1,18 +1,25 @@
-
 let quotes = [];
+let todayQuote;
 
 fetch("quotes.json")
     .then(res => res.json())
     .then(data => {
         quotes = data;
+
+        const today = new Date();
+        const start = new Date(today.getFullYear(), 0, 0);
+        const diff = today - start;
+        const oneDay = 1000 * 60 * 60 * 24;
+        const dayOfYear = Math.floor(diff / oneDay);
+        const quoteIndex = dayOfYear % quotes.length;
+
+        todayQuote = quotes[quoteIndex];
     });
 
 const button = document.getElementById("koalaButton");
 const quote = document.getElementById("quote");
 
 button.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomFact = quotes[randomIndex]
-    quote.textContent = randomFact;
+    quote.textContent = todayQuote;
 });
 
